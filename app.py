eeb17b4a9ec0410fea04e579fad18d7b2bd2073a
adivp7 <53617227+adivp7@users.noreply.github.com>
2022-07-02 22:57:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import subprocess
 import streamlit as st
-c1=subprocess.run("lspci",shell=True,capture_output=True)
-st.write(cl.output.decode())
+c=subprocess.run("lspci",shell=True,capture_output=True)
+st.write(c.output.decode())
