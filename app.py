32a6c36bd497d0fa4e78dc649b496d133837554e
adivp7 <53617227+adivp7@users.noreply.github.com>
2022-07-02 23:07:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
 import streamlit as st
-os.system("apt install -y hwinfo")
-c=subprocess.run("hwinfo",shell=True,capture_output=True)
+# os.system("apt install -y hwinfo")
+os.system("uname")
+c=subprocess.run("lscpu",shell=True,capture_output=True)
 st.write(c.stdout.decode())
 st.write("----end----")
