68700266e6e5851cf30aacbb36b77b958319e0ad
adivp7 <53617227+adivp7@users.noreply.github.com>
2022-07-04 22:53:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ import streamlit as st
 # os.system("apt install -y hwinfo")
 os.system("curl https://cdn.geekbench.com/Geekbench-5.4.5-Linux.tar.gz --output Geekbench-5.4.5-Linux.tar.gz")
 os.system("tar -xf Geekbench-5.4.5-Linux.tar.gz")
-c=subprocess.run("./Geekbench-5.4.5-Linux/geekbench5",capture_output=True)
-st.write(c.stdout.decode())
+os.system("./Geekbench-5.4.5-Linux/geekbench5")
 st.write("----end----")
